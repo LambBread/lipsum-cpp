41190ecef57e592e58153e10522f92dda2a15605
LambBread <judahjohnson4702@gmail.com>
2025-10-22 18:53:41 -0700

    Replace if-statement with generator expression

--- cmake/optcomp.cmake
+++ cmake/optcomp.cmake
@@ -1,9 +1,13 @@
-set(lipsum_srcs "")
+#set(lipsum_srcs "")
 if(LPSM_BUILD_STATIC)
     message("Building static")
-    list(APPEND lipsum_srcs "${CMAKE_CURRENT_SOURCE_DIR}/src/lipsum.cpp")
+    #list(APPEND lipsum_srcs "${CMAKE_CURRENT_SOURCE_DIR}/src/lipsum.cpp")
 endif()
 if(LPSM_BUILD_CWRAPPER)
     message("Building C wrapper")
-    list(APPEND lipsum_srcs "${CMAKE_CURRENT_SOURCE_DIR}/src/lipsum_h.cpp")
+    #list(APPEND lipsum_srcs "${CMAKE_CURRENT_SOURCE_DIR}/src/lipsum_h.cpp")
 endif()
+set(lipsum_srcs
+    "$<IF:$<BOOL:${LPSM_BUILD_STATIC}>,${CMAKE_CURRENT_SOURCE_DIR}/src/lipsum.cpp, \"\">"
+    "$<IF:$<BOOL:${LPSM_BUILD_CWRAPPER}>,${CMAKE_CURRENT_SOURCE_DIR}/src/lipsum_h.cpp, \"\">"
+)
